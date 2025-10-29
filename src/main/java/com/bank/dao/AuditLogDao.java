0eb4e9a45a35bbeb99ac943a5be08d605de748db
Swetha <116937727+SaiSwetha-0902@users.noreply.github.com>
2025-10-30 00:13:32 +0530

    Delete src/main/java/com/bank directory

--- src/main/java/com/bank/dao/AuditLogDao.java
+++
@@ -1,9 +0,0 @@
-package com.bank.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.bank.dao.entity.AuditLogEntity;
-
-public interface AuditLogDao extends JpaRepository<AuditLogEntity,Integer> {
-    
-}
